7ad9869768fbcce117e3071509d4ff453cef36a8
Biswajit Biswas <jfbiswajit@gmail.com>
2022-04-19 20:49:43 +0600

    Add: useEffect

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  // Whenever a state is change this function always will be get executed
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [msg, setMsg] = useState('Hello Baby!');
+
+  // This peace of code causes infinite loop
+  setTimeout(() => {
+    setMsg(Math.random().toString());
+  }, 1000);
+
+  console.log(msg);
+
+  // To get ride of this problem we use useEffect
 
   const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
     setIsLoggedIn(true);
   };
 
